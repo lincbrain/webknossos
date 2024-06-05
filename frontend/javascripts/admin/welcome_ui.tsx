71b246a5ca8b662cc4608eb8bb6f3243277ef241
Aaron Kanzer <aaronkanzer@dhcp-10-31-169-49.dyn.MIT.EDU>
2024-06-05 10:47:27 -0400

    resolve frontend yarn check

--- frontend/javascripts/admin/welcome_ui.tsx
+++ frontend/javascripts/admin/welcome_ui.tsx
@@ -75,7 +75,7 @@ export const WhatsNextHeader = ({ activeUser, onDismiss }: WhatsNextHeaderProps)
         <div className="text-and-button-container">
           <h1>Welcome to LINC | WEBKNOSSOS</h1>
           <p className="subtitle">
-            Welcome to the WEBKNOSSOS open-source version for <a href={"https://connects.mgh.harvard.edu/"} target={"_blank"}>LINC</a>.
+            Welcome to the WEBKNOSSOS open-source version for <a href={"https://connects.mgh.harvard.edu/"} target={"_blank"} rel={"noreferrer"}>LINC</a>.
             Congratulations on your new WEBKNOSSOS account! To hit the ground running, we recommend
             the following steps to you:
           </p>
