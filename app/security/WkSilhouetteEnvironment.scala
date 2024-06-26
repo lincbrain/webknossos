5696c2f0816dde1e1fbadca8d786a6bcff0677a8
Aaron Kanzer <aaronkanzer@Aarons-MacBook-Pro.local>
2024-06-26 14:30:14 -0400

    resolve build error

--- app/security/WkSilhouetteEnvironment.scala
+++ app/security/WkSilhouetteEnvironment.scala
@@ -29,7 +29,7 @@ class WkSilhouetteEnvironment @Inject()(
   private val cookieSettings = CookieAuthenticatorSettings(
     conf.Silhouette.CookieAuthenticator.cookieName,
     conf.Silhouette.CookieAuthenticator.cookiePath,
-    conf.Silhouette.CookieAuthenticator.domain,
+    Some(conf.Silhouette.CookieAuthenticator.domain),
     conf.Silhouette.CookieAuthenticator.secureCookie,
     conf.Silhouette.CookieAuthenticator.httpOnlyCookie,
     Some(Cookie.SameSite.Lax),
