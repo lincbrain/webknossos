c5d71f8ca81644d9a74a8f6112b0e448268f2834
Aaron Kanzer <aaronkanzer@Aarons-MacBook-Pro.local>
2024-07-30 16:19:41 -0400

    Add team as default during annotation creation

--- app/models/annotation/AnnotationService.scala
+++ app/models/annotation/AnnotationService.scala
@@ -400,6 +400,9 @@ class AnnotationService @Inject()(
       teamId <- selectSuitableTeam(user, dataset) ?~> "annotation.create.forbidden"
       annotation = Annotation(ObjectId.generate, datasetId, None, teamId, user._id, annotationLayers)
       _ <- annotationDAO.insertOne(annotation)
+      teamIdValidated = List(teamId)
+      _ <- Fox.serialCombined(teamIdsValidated)(teamDAO.findOne(_)) ?~> "updateSharedTeams.failed.accessingTeam"
+      _ <- this.updateTeamsForSharedAnnotation(annotation._id, teamIdValidated)
     } yield annotation
 
   def makeAnnotationHybrid(annotation: Annotation, organizationName: String, fallbackLayerName: Option[String])(
