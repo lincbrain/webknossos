794beb68680125a3fc2d174ccce34e6d3c195068
Aaron Kanzer <aaronkanzer@Aarons-MacBook-Pro-2.local>
2025-05-03 13:03:18 -0400

    one more linting

--- app/models/dataset/credential/CredentialService.scala
+++ app/models/dataset/credential/CredentialService.scala
@@ -27,15 +27,15 @@ class CredentialService @Inject()(credentialDAO: CredentialDAO, conf: WkConf) {
   ): Option[DataVaultCredential] =
     uri.getScheme match {
       case DataVaultService.schemeHttps | DataVaultService.schemeHttp =>
-        credentialIdentifier.map(username =>
-          HttpBasicAuthCredential(
-            uri.toString,
-            username,
-            credentialSecret.getOrElse(""),
-            userId.map(_.toString),
-            organizationId
-          )
-        )
+        credentialIdentifier.map(
+          username =>
+            HttpBasicAuthCredential(
+              uri.toString,
+              username,
+              credentialSecret.getOrElse(""),
+              userId.map(_.toString),
+              organizationId
+          ))
 
       case DataVaultService.schemeS3 =>
         val s3PrivateBucketConfigKeyword =
@@ -43,10 +43,8 @@ class CredentialService @Inject()(credentialDAO: CredentialDAO, conf: WkConf) {
         val isPrivateBucketEnabled =
           conf.get[Boolean]("webKnossos.s3PrivateBucketConfig.enabled")
 
-        if (
-          uri.toString.contains(s3PrivateBucketConfigKeyword) && isPrivateBucketEnabled
-        ) {
-          val s3AccessIdKey     = sys.env("AWS_ACCESS_KEY_ID")
+        if (uri.toString.contains(s3PrivateBucketConfigKeyword) && isPrivateBucketEnabled) {
+          val s3AccessIdKey = sys.env("AWS_ACCESS_KEY_ID")
           val s3SecretAccessKey = sys.env("AWS_ACCESS_KEY")
           Some(
             S3AccessKeyCredential(
@@ -75,21 +73,21 @@ class CredentialService @Inject()(credentialDAO: CredentialDAO, conf: WkConf) {
 
       case DataVaultService.schemeGS =>
         for {
-          secret      <- credentialSecret
-          secretJson  <- JsonHelper.parseAs[JsValue](secret).toOption
-        } yield GoogleServiceAccountCredential(
-          uri.toString,
-          secretJson,
-          userId.map(_.toString),
-          organizationId
-        )
+          secret <- credentialSecret
+          secretJson <- JsonHelper.parseAs[JsValue](secret).toOption
+        } yield
+          GoogleServiceAccountCredential(
+            uri.toString,
+            secretJson,
+            userId.map(_.toString),
+            organizationId
+          )
 
       case _ => None
     }
 
   def insertOne(credential: DataVaultCredential)(implicit
-      ec: ExecutionContext
-  ): Fox[ObjectId] = {
+                                                 ec: ExecutionContext): Fox[ObjectId] = {
     val _id = ObjectId.generate
     for {
       _ <- credential match {
